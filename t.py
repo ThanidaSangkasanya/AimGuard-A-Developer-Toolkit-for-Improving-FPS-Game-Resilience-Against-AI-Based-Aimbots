e0a2b3769ab4a188edd3e3ef6670a0b70457c80a
ThanidaSangkasanya <tsangkasanya@gmail.com>
2026-07-04 22:38:57 +0700

    delete wrong file

--- t.py
+++
@@ -1,4 +0,0 @@
-import torch 
-print("CUDA:", torch.cuda.is_available()) 
-ckpt = torch.load("pretrained_models/yolov5n.pt", map_location="cpu", weights_only=False) 
-print("Loaded keys:", list(ckpt.keys())) 
--- test_model.py
+++
@@ -1,5 +0,0 @@
-import torch, sys
-sys.path.insert(0, '.') 
-from models.common import DetectMultiBackend 
-model = DetectMultiBackend('pretrained_models/yolov5n.pt', device=torch.device('cpu')) 
-print('Model loaded OK') 
